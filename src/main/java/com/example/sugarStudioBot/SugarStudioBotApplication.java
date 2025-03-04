593c04571a1725d30df9c9f3075d1d8045c3df85
Владислав <akter.res.hak@gmail.com>
2025-03-04 20:08:02 +0700

    create

---
+++ src/main/java/com/example/sugarStudioBot/SugarStudioBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.sugarStudioBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SugarStudioBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SugarStudioBotApplication.class, args);
+	}
+
+}
